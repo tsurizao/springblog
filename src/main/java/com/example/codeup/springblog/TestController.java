613bd4bb0bcb78a36b9e4e0ddc6635fcedcc01cf
Chase Martinez <chasemartinez22@gmail.com>
2022-07-27 12:11:50 -0500

    Feat: mini exercise #2 completion

--- src/main/java/com/example/codeup/springblog/TestController.java
+++ src/main/java/com/example/codeup/springblog/TestController.java
@@ -3,12 +3,26 @@ package com.example.codeup.springblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
     @GetMapping("/firstview")
-    public String returnFirstView(Model vModel){
+    public String returnFirstView(Model vModel) {
         vModel.addAttribute("name", "Chase");
         return "first-view";
+    }
+
+    @GetMapping("/citysearch")
+    public String returnCitySearchForm() {
+        return "city-search";
+    }
+
+    @PostMapping("/citysearch")
+    public String returnCitySearchResults(@RequestParam String query, Model vmodel) {
+        vmodel.addAttribute("search", query);
+        return "city-search-results";
     }
 }
