45b94f6020e651bc32fda2983225948cae319dc4
Chase Martinez <chasemartinez22@gmail.com>
2022-07-27 16:54:41 -0500

    Feat: posts and index html files properly display information provided by the PostController
    Exercise complete

--- src/main/java/com/example/codeup/springblog/PostController.java
+++ src/main/java/com/example/codeup/springblog/PostController.java
@@ -1,32 +1,41 @@
 package com.example.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.constant.Constable;
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String getAllPosts(){
-        return "Posts index page.";
+    public String getAllPosts(Model model){
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Post 1", "The body for post 1");
+        Post post2 = new Post("Post 2", "The body of Post 2");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "/posts/index";
     }
 
-    @GetMapping("posts/{id}")
-    @ResponseBody
-    public String getPost(@PathVariable String id){
-        return "Viewing post id: " + id;
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable String id, Model model){
+        model.addAttribute("post", new Post("A title", "The body"));
+        return "/posts/show";
     }
 
-    @GetMapping("posts/create")
+    @GetMapping("/posts/create")
     @ResponseBody
     public String getCreatePostForm(){
         return "Form to create post...";
     }
 
-    @PostMapping("posts/create")
+    @PostMapping("/posts/create")
     @ResponseBody
     public String savePost(){
         return "Saving post...";
